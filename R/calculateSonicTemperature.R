95712a8bba31151a1f62c41d0b073ea3d99d9df9
Jerry Martin <Agriculturist@users.noreply.github.com>
2018-06-19 17:24:35 -0400

    added function for calculating sonic temperature

---
+++ R/calculateSonicTemperature.R
@@ -0,0 +1,20 @@
+#' calculateSonicTemperature 
+#'
+#' This function calculates the sonic virtual temperature in air based on the measured speed of sound.
+#' 
+#' Results are in degrees Celcius.
+#'
+#' @param speedOfSound the speed of sound in m/s
+#' @param GammaConstant ratio of specific heat of dry air at constant pressure to that at a constant volume
+#' @param Rd ideal gas constant for dry air (units J K^-1 kg^-1)
+#' @keywords emissions
+#' @examples
+#'
+#' @export
+#' 
+#' 
+#' 
+
+calculateSonicTemperature <- function(speedOfSound, GammaConstant = 1.4, Rd = 287.04){
+  return(speedOfSound^2 / (Gamma_const * R_d)  - 273.15)
+}
