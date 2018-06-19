0dca9610801326e6adbb815e1a054b8e7b6fd1c1
Jerry Martin <Agriculturist@users.noreply.github.com>
2018-06-19 17:39:25 -0400

    Add files via upload

---
+++ R/timeCeiling.R
@@ -0,0 +1,38 @@
+#' timeCeiling
+#'
+#' This function acts like a ceiling function for time.
+#'
+#' @param timeValue typically a column of time Values to take the floor function
+#' @param timeIntervalValue the value of the time interval
+#' @param timeIntervalUnits the units of the time interval
+#' @keywords time
+#' @examples
+#'
+#' @export
+
+timeCeiling <- function(timeValue, timeIntervalValue = 15, timeIntervalUnits = "minutes"){
+  
+  timeIntervalInSeconds <- switch(timeIntervalUnits,
+                                  "seconds" = 1,
+                                  "second" = 1,
+                                  "sec" = 1,
+                                  "s" = 1,
+                                  "minutes" = 60,
+                                  "minute" = 60,
+                                  "min" = 60,
+                                  "m" = 60,
+                                  "hours" = 3600,
+                                  "hour" = 3600,
+                                  "hr" = 3600,
+                                  "h" = 3600)
+  
+  
+  return(
+    as.POSIXct(
+      floor(as.numeric(as.POSIXct(timeValue)) / 
+              (timeIntervalValue * timeIntervalInSeconds)) * 
+        (timeIntervalValue * timeIntervalInSeconds) 
+      + timeIntervalValue * timeIntervalInSeconds, 
+      origin='1970-01-01'))
+  
+}
