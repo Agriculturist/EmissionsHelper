c97b108d1146d1b93255356f1023cba3a59fe2c6
Jerry Martin <Agriculturist@users.noreply.github.com>
2018-06-19 16:58:06 -0400

    no longer assumes American date format
    This file was modified so it does not assume American date format.

--- R/reformatTime.R
+++ R/reformatTime.R
@@ -1,22 +1,188 @@
 #' reformatTime
 #'
-#' This function reformats time so it is always in a consistant format.
-#'
-#' This function is useful for importing date from a text file
-#'
-#' @param cdate the date an event occured
-#' @param ctime the time of an event
+#' This function is a wrapper for the as.POSIXct function. This function is 
+#' designed require less effort.   
+#' 
+#' The format parameter can be identical to the as.POSIXct function parameter
+#' or it can be simplified to simply "American", "British", or "International". 
+#' 
+#' The function identifies the format from knowledge of the order.
+#' 
+#' The user identifies which format order.  This code will issue a warning 
+#' if an obvious issue occurs with the format.  If the dataset is large
+#' getting the format is critical as testing is limited to the first 1000 datapoints. 
+#'   
+#' format = "American"      ... date order is (1) month (2) day (3) year
+#' format = "British"       ... date order is (1) day (2) month (3) year
+#' format = "International" ... date order is (1) year (2) month (3) day
+#' 
+#' The time of day may not be part of the character string included with the
+#' date.  If it is not then the timeIfSeparateFromDate paramter can include 
+#' a text string with the time. 
+#' 
+#' @param dateToReformat the date and or time an event occured
+#' @param timeIfSeparateFromDate the time of an event as a character array 
 #' @param format format string to output results
 #' @param tz time zone to use
 #' @keywords time
 #' @examples
-#'
+#'  
+#'    reformatTime()
 #' @export
 
-reformatTime <- function(cdate, ctime = NULL, format = "%m/%d/%Y %H:%M:%S", tz = "Etc/GMT+6"){
-  if(is.null(ctime)){
-    as.POSIXct(strptime(as.character(cdate), format = format))
+reformatTime <- function(dateToReformat, 
+                         timeIfSeparateFromDate = NULL, 
+                         dateFormat = "American", 
+                         tz = "Etc/GMT+6",
+                         dateTimeDelimiter = "//s"){
+  
+  lowerFormat <- tolower(dateFormat)
+  
+  if(lowerFormat %in% c("american","british","international")){
+    
+    separator <- if(grepl("[/]", dateToReformat[1]) > 0){"/"}else{
+      if(grepl("[/.]", dateToReformat[1]) > 0){"."}else{
+        if(grepl("[-]", dateToReformat[1]) > 0){"-"}else{" "}
+      }
+    }
+    
+    sepRegEx <- "[\\/\\.\\-]"
+    dayRegEx <- "[0-3]?[0-9]"
+    monthRegEx <- "[0-1]?[0-9]"
+    yearRegEx <- "[0-2]?[0-9]?[0-9][0-9]"
+    fullyearRegEx <- "[0-2][0-9][0-9][0-9]"
+    HHMMSSRegEx <- "[0-2]?[0-9]:[0-6]?[0-9]:[0-6]?[0-9]"
+    
+    testDates <- function(testDateData, ...) {
+      if (length(testDateData) < 1000){ # if dataset is big it only tests with first 1000 points
+       return(all(as.logical(sapply(testDateData, function(x){
+          grepl(paste0(...), x)}))))
+      }else{
+        return(all(as.logical(sapply(testDateData[1:999], function(x){
+          grepl(paste0(...), x)}))))    
+      }
+    }
+    
+    isAmericanDateTime <- testDates(
+      dateToReformat, 
+      "^", monthRegEx, sepRegEx, dayRegEx, sepRegEx, yearRegEx, 
+      dateTimeDelimiter, HHMMSSRegEx, "$")
+    
+    isAmericanDate <- testDates(
+      dateToReformat,
+      "^", monthRegEx, sepRegEx, dayRegEx, sepRegEx, yearRegEx, 
+      "$")
+    
+
+    isBritishDateTime <- testDates(
+      dateToReformat, 
+      "^", dayRegEx, sepRegEx, monthRegEx, sepRegEx, yearRegEx, 
+      dateTimeDelimiter, HHMMSSRegEx, "$")
+  
+    isBritishDate <- testDates(
+      dateToReformat, 
+      "^", dayRegEx, sepRegEx, monthRegEx, sepRegEx, yearRegEx, 
+      "$")
+
+    isInternationalDateTime <- testDates(
+      dateToReformat, 
+      "^", yearRegEx, sepRegEx, monthRegEx, sepRegEx, dayRegEx, 
+      dateTimeDelimiter, HHMMSSRegEx, "$")
+      
+    isInternationalDate <- testDates(
+      dateToReformat, 
+      "^", yearRegEx, sepRegEx, monthRegEx, sepRegEx, dayRegEx, 
+      "$")
+    
+    if(lowerFormat %in% "american"){
+      
+      if (!(isAmericanDateTime || isAmericanDate)){
+        warning("The date does not appear to be formatted as an American date.")
+      }  
+      
+      if (isAmericanDateTime){
+        if (grepl(paste0("^", monthRegEx, sepRegEx, dayRegEx, sepRegEx, 
+                        fullyearRegEx, dateTimeDelimiter, HHMMSSRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%m", separator,  "%d" , separator, "%Y",  
+                                   dateTimeDelimiter, "%H:%M:%S")           
+        }else{
+          modifiedFormat = paste0( "%m", separator,  "%d" , separator, "%y",  
+                                   dateTimeDelimiter, "%H:%M:%S")             
+        }
+      }
+      
+      if(isAmericanDate){
+        if (grepl(paste0("^", monthRegEx, sepRegEx, dayRegEx, sepRegEx, 
+                        fullyearRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%m", separator,  "%d" , separator, "%Y")           
+        }else{
+          modifiedFormat = paste0( "%m", separator,  "%d" , separator, "%y")            
+        }
+      }
+    }
+    
+    if(lowerFormat %in% "british"){
+      if (!(isBritishDateTime || isBritishDate )){
+        warning("The date does not appear to be formatted as a British date.")
+      }  
+      
+      if (isBritishDateTime){
+        if (grepl(paste0("^", dayRegEx, sepRegEx, monthRegEx, sepRegEx,  
+                         fullyearRegEx, dateTimeDelimiter, HHMMSSRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%d", separator,  "%m" , separator, "%Y",  
+                                   dateTimeDelimiter, "%H:%M:%S")
+        }else{
+          modifiedFormat = paste0( "%d", separator,  "%m" , separator, "%y",  
+                                   dateTimeDelimiter, "%H:%M:%S")
+        }
+      } 
+      
+      if(isBritishDate){
+        if (grepl(paste0("^", dayRegEx, sepRegEx, monthRegEx, sepRegEx,  
+                        fullyearRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%d", separator,  "%m" , separator, "%Y")         
+        }else{
+          modifiedFormat = paste0( "%d", separator,  "%m" , separator, "%Y")            
+        }
+      }      
+      
+      
+      
+    }    
+    
+    if(lowerFormat %in% "international"){
+      if (!(isInternationalDateTime || isInternationalDate)){
+        warning("The date does not appear to be formatted as an International date.")
+      }       
+      
+      if (isInternationalDateTime){
+        if (grepl(paste0("^", fullyearRegEx, sepRegEx, monthRegEx, sepRegEx,
+                        dayRegEx, dateTimeDelimiter, HHMMSSRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%Y", separator,  "%m" , separator, "%d",  
+                                   dateTimeDelimiter, "%H:%M:%S")         
+        }else{
+          modifiedFormat = paste0( "%y", separator,  "%m" , separator, "%d",  
+                                   dateTimeDelimiter, "%H:%M:%S")             
+        }
+      } 
+      
+      if(isInternationalDate){
+        if (grepl(paste0("^", fullyearRegEx, sepRegEx, monthRegEx, sepRegEx,
+                        dayRegEx, "$"), dateToReformat)){
+          modifiedFormat = paste0( "%Y", separator,  "%m" , separator, "%d")           
+        }else{
+          modifiedFormat = paste0( "%y", separator,  "%m" , separator, "%d")             
+        }
+      }     
+      
+    }     
   }else{
-    as.POSIXct(strptime(mapply(paste, as.character(cdate), as.character(ctime)), 
-                        format = format, tz = tz))
-  }}
+    modifiedFormat <- dateFormat
+  }
+  modifiedFormat <<- modifiedFormat 
+  
+  
+  return(as.POSIXct(strptime(as.character(dateToReformat), format = modifiedFormat)))
+  
+}
+
