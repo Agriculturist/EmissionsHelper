1fa8252bf31daa77a64071005a2e0a08c46037cd
Jerry Martin <Agriculturist@users.noreply.github.com>
2018-06-25 11:22:57 -0400

    Update reformatTime.R
    Updated documentation.. Changed the function to better handle indeterminate dates

--- R/reformatTime.R
+++ R/reformatTime.R
@@ -10,11 +10,19 @@
 #' 
 #' The user identifies which format order.  This code will issue a warning 
 #' if an obvious issue occurs with the format.  If the dataset is large
-#' getting the format is critical as testing is limited to the first 1000 datapoints. 
+#' getting the format is critical as testing is limited to the first 100 datapoints. 
+#' 
+#' testChararacterDate parameters may be entered into the function
+#' 
+#' If dateformat is blank the system will guess at the date format.
 #'   
-#' format = "american"      ... date order is (1) mm (2) day (3) year
-#' format = "british"       ... date order is (1) day (2) mm (3) year
-#' format = "international" ... date order is (1) year (2) mm (3) day
+#' Because the American and British styles can be confused it may be necissary
+#' to specify the format.
+#'
+#' dateformat = "american"      ... date order is (1) mm (2) day (3) year
+#' dateformat = "british"       ... date order is (1) day (2) mm (3) year
+#' dateformat = "international" ... date order is (1) year (2) mm (3) day
+#' 
 #' 
 #' The time of day may not be part of the character string included with the
 #' date.  If it is not then the timeIfSeparateFromDate paramter can include 
@@ -25,7 +33,7 @@
 #' 
 #' @param dateToReformat the date and or time an event occured
 #' @param timeIfSeparateFromDate the time of an event as a character array 
-#' @param format format string to output results
+#' @param dateformat either ("american", "british", "international") or the strptime format parameter. 
 #' @param tz time zone to use
 #' @keywords time
 #' @examples
@@ -35,7 +43,7 @@
 
 reformatTime <- function(dateToReformat, 
                          timeIfSeparateFromDate = NULL, 
-                         dateFormat = "american", 
+                         dateFormat = NULL, 
                          tz = "Etc/GMT+6",
                          ...
                          ){
@@ -52,13 +60,24 @@ reformatTime <- function(dateToReformat,
   dateFormat <- tolower(as.character(dateFormat))
   
 
-  dateInfo      <- testCharacterDate (dateToReformat, ...)
-  dateseparator <- dateInfo$dateSeparator
-  dateTimeSeparator <- dateInfo$dateTimeSeparator
-  formatStyle   <- tolower(dateInfo$formatStyle)
-  isFullYear    <- dateInfo$yearFormat == "Full"
-  isDateTime    <- dateInfo$timePresent
- 
+  if (length(dateFormat) == 0 ||  
+      isTRUE(tolower(dateFormat) %in% 
+             c("american", "british", "international"))){
+    
+    
+    dateInfo      <- testCharacterDate (dateToReformat, ...)
+    dateseparator <- dateInfo$dateSeparator
+    dateTimeSeparator <- dateInfo$dateTimeSeparator
+    if (length(dateFormat) == 0){
+      formatStyle <- tolower(dateInfo$formatStyle)
+    }else{
+      formatStyle <- dateFormat
+    }    
+    isFullYear    <- dateInfo$yearFormat == "Full"
+    isDateTime    <- dateInfo$timePresent
+
+    
+  }
     
   # assemble the format expression for the as.POSIXct function
   formatForPOSIXct <- function(whichFormat, isDateTime, isFullYear){
@@ -88,11 +107,15 @@ reformatTime <- function(dateToReformat,
   if (formatStyle != "unknown" && !is.null(dateFormat)){
     modifiedFormat <- formatForPOSIXct(formatStyle, isDateTime, isFullYear)
   }else{
-    modifiedFormat <- dateFormat
+    if (length(dateFormat) == 0){
+      stop(paste("Date format is indeterminant.",
+                    "Try setting the dateformat parameter."))
+    }else{
+      modifiedFormat <- dateFormat
+    }
   }
 
   return(as.POSIXct(strptime(as.character(dateToReformat), 
                              format = modifiedFormat)))
   
 }
-
